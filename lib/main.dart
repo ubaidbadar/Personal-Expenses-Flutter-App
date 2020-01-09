8704ea6d1676502b16ab4e1e9fdaceceda8ccf42
Ubaid Badar <m.ubaidbadar@gmail.com>
2020-01-09 11:46:57 +0500

    add_basic_theme_configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personalexpenses/widgets/home-page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: MyHomePage(),
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+            fontFamily: 'Quicksand',
+            fontWeight: FontWeight.bold,
+            fontSize: 18
+          )
+        )
+      ),
     );
   }
 }
